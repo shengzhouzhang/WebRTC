ee5821d24f0a31ef0e5e068df784be8871b3cc58
Steven Zhang <steven@cammy.com>
2014-11-01 14:58:57 +1100

    style incident page

--- public/templates/incident.react.jsx
+++ public/templates/incident.react.jsx
@@ -74,30 +74,38 @@ define(['dispatcher', 'actions', 'incident.store'],
     render: function () {
 
       var events = _.map(this.state.events, function (event, index) {
+        console.log(event);
+
         // TODO: need to have unique id
         return (
           <Event key={event.id + index} start={event.start} end={event.end} snapshots={event.snapshots} />
         );
       }.bind(this));
 
-
       var contacts = _.map(this.state.contact, function (contact, index) {
         return (
           <div key={this.state.home_alarm_id + '_contact_' + index}>
-            <span>{contact.name}</span><span>{contact.phone}</span><span>{contact.relationship}</span>
+            <span>{contact.name}</span><span>{contact.relationship}</span><span>{contact.phone}</span>
           </div>
         );
       }.bind(this));
 
+      var cover = !!this.state.events && !!this.state.events.length ? this.state.events[0].cover : '';
+
       return (
         <div>
-          <div className="address">
-            <div><label>Address</label></div>
-            <div><span>200 Broadway Av</span><span>NSW</span><span>2112</span><span>Australia</span></div>
-          </div>
-          <div className="contact">
-            <div><label>Contact</label></div>
-            {contacts}
+          <div className="header">
+            <div className="cover" style={{backgroundImage: 'url(' + cover + ')'}}></div>
+            <div className="address">
+              <div><span>200 Broadway Av</span><span>NSW</span><span>2112</span><span>Australia</span></div>
+            </div>
+            <div className="contact">
+              <div><label>Contact Info</label></div>
+              {contacts}
+            </div>
+            <div className="actions">
+              <a href="#">Close</a>
+            </div>
           </div>
           <div className="events">{events}</div>
         </div>
