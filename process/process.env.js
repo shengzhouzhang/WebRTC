dad817d8df3951b65299e3bb5182b6908b5afc53
Steven Zhang <steven@cammy.com>
2014-11-21 15:53:37 +1100

    add redis production

--- process/process.env.js
+++ process/process.env.js
@@ -12,9 +12,9 @@ var Env = {
 
     switch(env.NODE_ENV){
       case 'production':
-        env.REDIS_PORT = 10023;
-        env.REDIS_HOST = 'pub-redis-10023.us-east-1-4.3.ec2.garantiadata.com';
-        env.REDIS_PASSWORD = 'PtxrZAx9qJUJqV4d';
+        env.REDIS_PORT = 17702;
+        env.REDIS_HOST = 'pub-redis-17702.us-east-1-2.5.ec2.garantiadata.com';
+        env.REDIS_PASSWORD = 'FLFUeBBXbPaDh2Hy';
         env.MONGODB_URI = 'mongodb://cammy:cammy@galaga.2.mongolayer.com:10011,galaga.3.mongolayer.com:10008/cammy-irc-production';
         break;
 
