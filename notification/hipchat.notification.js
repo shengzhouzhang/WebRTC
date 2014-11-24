5a34d270b37b8c980ee162da63059f6eb7bd4743
Steven Zhang <steven@cammy.com>
2014-11-25 10:01:21 +1100

    change hipchat notification

--- notification/hipchat.notification.js
+++ notification/hipchat.notification.js
@@ -36,7 +36,7 @@ dispatcher.register(dispatcher.actions.NEW_INCIDENT, function (incident) {
 
 dispatcher.register(dispatcher.actions.STATUS_UPDATED, function (incident) {
   if(!incident) { return; }
-  // if(!process.env.HIPCHAT) { return; }
+  if(!process.env.HIPCHAT) { return; }
 
   var messages = [];
 
@@ -58,7 +58,6 @@ dispatcher.register(dispatcher.actions.STATUS_UPDATED, function (incident) {
     messages.push(note.created_by);
     messages.push(moment(note.created_at).format('YYYY-MM-DD HH:mm:ss'));
   }
-
 
   hc.sendMessage(messages.join('<br/>'), function (err, result) {
     if(!!err) { logger.error(err.stack || err); }
