76a91fceea44cc2857c85007bfc104cc005cd6c0
Steven Zhang <steven@cammy.com>
2014-11-26 11:39:36 +1100

    add create user scripts

---
+++ test/scripts/local/create.user.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -d '{"username": "brice@cammy.com", "password": "s6madr"}' http://localhost:3000/user?access_token=542222e11fe800ef1332a6a6
---
+++ test/scripts/production/create.user.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -d '{"username": "brice@cammy.com", "password": "s6madr"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
