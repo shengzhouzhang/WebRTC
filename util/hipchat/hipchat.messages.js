d317c034a316073fb5523b4289998e924488fc6f
Steven Zhang <steven@cammy.com>
2014-11-26 11:38:40 +1100

    change room id

--- util/hipchat/hipchat.messages.js
+++ util/hipchat/hipchat.messages.js
@@ -6,7 +6,7 @@ var util = require('util'),
     logger = require('../log/application.log').logger;
 
 
-var uri = 'https://api.hipchat.com/v1/rooms/message?room_id=828046&color=red&from=IRC-Alert&message=<a href=\"%s\">%s</a>&message_format=html&notify=1&auth_token=e22bdaa96c91ec7b2bb3df8434d4a6';
+var uri = 'https://api.hipchat.com/v1/rooms/message?room_id=1006721&color=red&from=IRC-Alert&message=<a href=\"%s\">%s</a>&message_format=html&notify=1&auth_token=e22bdaa96c91ec7b2bb3df8434d4a6';
 
 var sendMessage = function (msg, cb) {
 
