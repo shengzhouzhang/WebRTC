8409c1ff30a1c9a996d4fadf87f63355f7527003
Steven Zhang <steven@cammy.com>
2014-11-27 15:20:07 +1100

    fixed dispatcher bug

--- dispatcher/dispatcher.js
+++ dispatcher/dispatcher.js
@@ -4,16 +4,7 @@
 var _ = require('lodash'),
     Promise = require('promise');
 
-var _handlers = {},
-    _promises = [];
-
-var _addPromise = function (handler, options) {
-  _promises.push(handler(options));
-};
-
-var _clearPromises = function () {
-  _promises = [];
-};
+var _handlers = {};
 
 var dispatcher = {
 
@@ -50,9 +41,11 @@ var dispatcher = {
   dispatch: function (action, options) {
     if(!action) { throw new Error ('missing values'); }
 
+    var _promises = [];
+
     _.each(_handlers[action], function (handler) {
       try {
-        _addPromise(handler, options);
+        _promises.push(handler(options));
       } catch (err) {
         console.log(err.stack || err);
       }
@@ -60,12 +53,11 @@ var dispatcher = {
 
     return Promise.all(_promises).then(
       function (result) {
-        _clearPromises();
         if(!result.length) { return null; }
         if(result.length === 1) { return result[0]; }
         return result;
       },
-      function (error) { _clearPromises(); return error; }
+      function (error) { console.log(error.stack || error); return error; }
     );
   }
 };
