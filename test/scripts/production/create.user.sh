e8b1ab67359ad88f1031c61ccae95ca0004c2d71
Steven Zhang <steven@cammy.com>
2014-11-27 13:35:59 +1100

    remove user list

--- test/scripts/production/create.user.sh
+++
@@ -1,9 +0,0 @@
-curl -H "Content-Type: application/json" -d '{"username": "brice@cammy.com", "password": "s6madr"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
-curl -H "Content-Type: application/json" -d '{"username": "jimmy@cammy.com", "password": "Jimmyz0r!"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
-curl -H "Content-Type: application/json" -d '{"username": "christian@cammy.com", "password": "Hello1"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
-curl -H "Content-Type: application/json" -d '{"username": "chelsea@cammy.com", "password": "chels007"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
-curl -H "Content-Type: application/json" -d '{"username": "jason@cammy.com", "password": "scream"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
-curl -H "Content-Type: application/json" -d '{"username": "sarah@cammy.com", "password": "Amyrose4"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
-curl -H "Content-Type: application/json" -d '{"username": "jonathan@cammy.com", "password": "cYyNkA"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
-curl -H "Content-Type: application/json" -d '{"username": "tony@cammy.com", "password": "5KbWCn"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
-curl -H "Content-Type: application/json" -d '{"username": "dom@cammy.com", "password": "rubymay2"}' http://irc.cammy.com/user?access_token=542222e11fe800ef1332a6a6
